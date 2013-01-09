baae6d6870a2ae472af36e123cbfe08c19e2f779
Mike Bostock <mbostock@gmail.com>
2013-01-09 13:41:51 -0800

    Bah, add missing semicolon.

--- d3.geo.projection.v0.js
+++ d3.geo.projection.v0.js
@@ -50,17 +50,17 @@
     return projection(aitoff);
   }).raw = aitoff;
   function guyou(λ, φ) {
-    return ellipticFi(λ, sgn(φ) * Math.log(Math.tan(.5 * (Math.abs(φ) + π / 2))), .5);
+    return guyouEllipticFi(λ, sgn(φ) * Math.log(Math.tan(.5 * (Math.abs(φ) + π / 2))), .5);
   }
-  function ellipticFi(φ, ψ, m) {
+  function guyouEllipticFi(φ, ψ, m) {
     var r = Math.abs(φ), i = Math.abs(ψ), sinhψ = .5 * ((sinhψ = Math.exp(i)) - 1 / sinhψ);
     if (r) {
       var cscφ = 1 / Math.sin(r), cotφ2 = (cotφ2 = Math.cos(r) * cscφ) * cotφ2, b = -(cotφ2 + m * (sinhψ * sinhψ * cscφ * cscφ + 1) - 1), cotλ2 = .5 * (-b + Math.sqrt(b * b - 4 * (m - 1) * cotφ2));
-      return [ ellipticF(Math.atan(1 / Math.sqrt(cotλ2)), m) * sgn(φ), ellipticF(Math.atan(Math.sqrt(cotλ2 / cotφ2 - 1) / m), 1 - m) * sgn(ψ) ];
+      return [ guyouEllipticF(Math.atan(1 / Math.sqrt(cotλ2)), m) * sgn(φ), guyouEllipticF(Math.atan(Math.sqrt(cotλ2 / cotφ2 - 1) / m), 1 - m) * sgn(ψ) ];
     }
-    return [ 0, ellipticF(Math.atan(sinhψ), 1 - m) * sgn(ψ) ];
+    return [ 0, guyouEllipticF(Math.atan(sinhψ), 1 - m) * sgn(ψ) ];
   }
-  function ellipticF(φ, m) {
+  function guyouEllipticF(φ, m) {
     var a = 1, b = Math.sqrt(1 - m), c = Math.sqrt(m);
     for (var i = 0; Math.abs(c) > ε; i++) {
       if (φ % π) {
@@ -114,7 +114,8 @@
   }
   sinuMollweide.invert = function(x, y) {
     return y > -sinuMollweideφ ? mollweide.invert(x, y - sinuMollweideY) : sinusoidal.invert(x, y);
-  }(d3.geo.sinuMollweide = function() {
+  };
+  (d3.geo.sinuMollweide = function() {
     return projection(sinuMollweide).rotate([ -20, -55 ]);
   }).raw = sinuMollweide;
   function armadillo(φ0) {
