9928460255a8487ffa0c69a6745acf7fcc446293
Mike Bostock <mbostock@gmail.com>
2013-01-10 16:35:01 -0800

    Update d3.geo.projection.

--- d3.geo.projection.v0.js
+++ d3.geo.projection.v0.js
@@ -180,18 +180,27 @@
     return projection(sinuMollweide).rotate([ -20, -55 ]);
   }).raw = sinuMollweide;
   function armadillo(φ0) {
-    var sinφ0 = Math.sin(φ0), cosφ0 = Math.cos(φ0), tanφ0 = Math.tan(φ0), k = (1 + sinφ0 - cosφ0) / 2;
+    var sinφ0 = Math.sin(φ0), cosφ0 = Math.cos(φ0), sφ0 = φ0 > 0 ? 1 : -1, tanφ0 = Math.tan(sφ0 * φ0), k = (1 + sinφ0 - cosφ0) / 2;
     function forward(λ, φ) {
-      var cosφ = Math.cos(φ), cosλ = Math.cos(λ /= 2), φ1 = -Math.atan2(cosλ, tanφ0);
-      return [ (1 + cosφ) * Math.sin(λ), (φ > φ1 - .001 ? 0 : -1) + k + Math.sin(φ) * cosφ0 - (1 + cosφ) * sinφ0 * cosλ ];
+      var cosφ = Math.cos(φ), cosλ = Math.cos(λ /= 2);
+      return [ (1 + cosφ) * Math.sin(λ), (sφ0 * φ > -Math.atan2(cosλ, tanφ0) - .001 ? 0 : -sφ0 * 10) + k + Math.sin(φ) * cosφ0 - (1 + cosφ) * sinφ0 * cosλ ];
     }
+    forward.invert = function(x, y) {
+      var λ = 0, φ = 0, i = 50;
+      do {
+        var cosλ = Math.cos(λ), sinλ = Math.sin(λ), cosφ = Math.cos(φ), sinφ = Math.sin(φ), A = 1 + cosφ, fx = A * sinλ - x, fy = k + sinφ * cosφ0 - A * sinφ0 * cosλ - y, δxδλ = .5 * A * cosλ, δxδφ = -sinλ * sinφ, δyδλ = .5 * sinφ0 * A * sinλ, δyδφ = cosφ0 * cosφ + sinφ0 * cosλ * sinφ, denominator = δxδφ * δyδλ - δyδφ * δxδλ, δλ = .5 * (fy * δxδφ - fx * δyδφ) / denominator, δφ = (fx * δyδλ - fy * δxδλ) / denominator;
+        λ -= δλ, φ -= δφ;
+      } while ((Math.abs(δλ) > ε || Math.abs(δφ) > ε) && --i > 0);
+      return sφ0 * φ > -Math.atan2(Math.cos(λ), tanφ0) - .001 ? [ λ * 2, φ ] : null;
+    };
     return forward;
   }
   function armadilloProjection() {
-    var φ0 = π / 9, tanφ0 = Math.tan(φ0), m = projectionMutator(armadillo), p = m(φ0), stream_ = p.stream;
+    var φ0 = π / 9, sφ0 = φ0 > 0 ? 1 : -1, tanφ0 = Math.tan(sφ0 * φ0), m = projectionMutator(armadillo), p = m(φ0), stream_ = p.stream;
     p.parallel = function(_) {
       if (!arguments.length) return φ0 / π * 180;
-      return m(φ0 = _ * π / 180);
+      tanφ0 = Math.tan((sφ0 = (φ0 = _ * π / 180) > 0 ? 1 : -1) * φ0);
+      return m(φ0);
     };
     p.stream = function(stream) {
       var rotate = p.rotate(), rotateStream = stream_(stream), sphereStream = (p.rotate([ 0, 0 ]), 
@@ -199,9 +208,9 @@
       p.rotate(rotate);
       rotateStream.sphere = function() {
         sphereStream.polygonStart(), sphereStream.lineStart();
-        for (var λ = -180; λ < 180; λ += 90) sphereStream.point(λ, 90);
-        while (--λ >= -180) {
-          sphereStream.point(λ, -Math.atan2(Math.cos(λ * radians / 2), tanφ0) * degrees);
+        for (var λ = sφ0 * -180; sφ0 * λ < 180; λ += sφ0 * 90) sphereStream.point(λ, sφ0 * 90);
+        while (sφ0 * (λ -= φ0) >= -180) {
+          sphereStream.point(λ, sφ0 * -Math.atan2(Math.cos(λ * radians / 2), tanφ0) * degrees);
         }
         sphereStream.lineEnd(), sphereStream.polygonEnd();
       };
@@ -766,12 +775,24 @@
     var λ2 = λ * λ, φ2 = φ * φ;
     return [ λ * (.975534 + φ2 * (-.119161 + λ2 * -.0143059 + φ2 * -.0547009)), φ * (1.00384 + λ2 * (.0802894 + φ2 * -.02855 + λ2 * 199025e-9) + φ2 * (.0998909 + φ2 * -.0491032)) ];
   }
+  laskowski.invert = function(x, y) {
+    var λ = x, φ = y, i = 50;
+    do {
+      var λ2 = λ * λ, φ2 = φ * φ, λφ = λ * φ, fx = λ * (.975534 + φ2 * (-.119161 + λ2 * -.0143059 + φ2 * -.0547009)) - x, fy = φ * (1.00384 + λ2 * (.0802894 + φ2 * -.02855 + λ2 * 199025e-9) + φ2 * (.0998909 + φ2 * -.0491032)) - y, δxδλ = .975534 - φ2 * (.119161 + 3 * λ2 * .0143059 + φ2 * .0547009), δxδφ = -λφ * (2 * .119161 + 4 * .0547009 * φ2 + 2 * .0143059 * λ2), δyδλ = λφ * (2 * .0802894 + 4 * 199025e-9 * λ2 + 2 * -.02855 * φ2), δyδφ = 1.00384 + λ2 * (.0802894 + 199025e-9 * λ2) + φ2 * (3 * (.0998909 - .02855 * λ2) - 5 * .0491032 * φ2), denominator = δxδφ * δyδλ - δyδφ * δxδλ, δλ = (fy * δxδφ - fx * δyδφ) / denominator, δφ = (fx * δyδλ - fy * δxδλ) / denominator;
+      λ -= δλ, φ -= δφ;
+    } while ((Math.abs(δλ) > ε || Math.abs(δφ) > ε) && --i > 0);
+    return [ λ, φ ];
+  };
   (d3.geo.laskowski = function() {
     return projection(laskowski);
   }).raw = laskowski;
   function littrow(λ, φ) {
     return [ Math.sin(λ) / Math.cos(φ), Math.tan(φ) * Math.cos(λ) ];
   }
+  littrow.invert = function(x, y) {
+    var x2 = x * x, y2 = y * y, y2_1 = y2 + 1, cosφ = x ? Math.SQRT1_2 * Math.sqrt((y2_1 - Math.sqrt(x2 * x2 + 2 * x2 * (y2 - 1) + y2_1 * y2_1)) / x2 + 1) : 1 / Math.sqrt(y2_1);
+    return [ asin(x * cosφ), sgn(y) * acos(cosφ) ];
+  };
   (d3.geo.littrow = function() {
     return projection(littrow);
   }).raw = littrow;
