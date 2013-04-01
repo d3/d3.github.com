c137c1208edbd74a8c4433389ea05079dbd4d883
Mike Bostock <mbostock@gmail.com>
2013-04-01 16:09:02 -0700

    Update d3.geo.projection.

--- d3.geo.projection.v0.js
+++ d3.geo.projection.v0.js
@@ -478,6 +478,68 @@
   (d3.geo.gallStereographic = function() {
     return projection(gallStereographic);
   }).raw = gallStereographic;
+  function ginzburgPolyconic(a, b, c, d, e, f, g, h) {
+    if (arguments.length < 8) h = 0;
+    function forward(λ, φ) {
+      if (!φ) return [ a * λ / π, 0 ];
+      var φ2 = φ * φ, xB = a + φ2 * (b + φ2 * (c + φ2 * d)), yB = φ * (e - 1 + φ2 * (f - h + φ2 * g)), m = (xB * xB + yB * yB) / (2 * yB), α = λ * Math.asin(xB / m) / π;
+      return [ m * Math.sin(α), φ * (1 + φ2 * h) + m * (1 - Math.cos(α)) ];
+    }
+    forward.invert = function(x, y) {
+      var λ = π * x / a, φ = y, δλ, δφ, i = 50;
+      do {
+        var φ2 = φ * φ, xB = a + φ2 * (b + φ2 * (c + φ2 * d)), yB = φ * (e - 1 + φ2 * (f - h + φ2 * g)), p = xB * xB + yB * yB, q = 2 * yB, m = p / q, m2 = m * m, dαdλ = Math.asin(xB / m) / π, α = λ * dαdλ;
+        xB2 = xB * xB, dxBdφ = (2 * b + φ2 * (4 * c + φ2 * 6 * d)) * φ, dyBdφ = e + φ2 * (3 * f + φ2 * 5 * g), 
+        dpdφ = 2 * (xB * dxBdφ + yB * (dyBdφ - 1)), dqdφ = 2 * (dyBdφ - 1), dmdφ = (dpdφ * q - p * dqdφ) / (q * q), 
+        cosα = Math.cos(α), sinα = Math.sin(α), mcosα = m * cosα, msinα = m * sinα, dαdφ = λ / π * (1 / asqrt(1 - xB2 / m2)) * (dxBdφ * m - xB * dmdφ) / m2, 
+        fx = msinα - x, fy = φ * (1 + φ2 * h) + m - mcosα - y, δxδφ = dmdφ * sinα + mcosα * dαdφ, 
+        δxδλ = mcosα * dαdλ, δyδφ = 1 + dmdφ - (dmdφ * cosα - msinα * dαdφ), δyδλ = msinα * dαdλ, 
+        denominator = δxδφ * δyδλ - δyδφ * δxδλ;
+        if (!denominator) break;
+        λ -= δλ = (fy * δxδφ - fx * δyδφ) / denominator;
+        φ -= δφ = (fx * δyδλ - fy * δxδλ) / denominator;
+      } while ((Math.abs(δλ) > ε || Math.abs(δφ) > ε) && --i > 0);
+      return [ λ, φ ];
+    };
+    return forward;
+  }
+  var ginzburg4 = ginzburgPolyconic(2.8284, -1.6988, .75432, -.18071, 1.76003, -.38914, .042555);
+  (d3.geo.ginzburg4 = function() {
+    return projection(ginzburg4);
+  }).raw = ginzburg4;
+  var ginzburg5 = ginzburgPolyconic(2.583819, -.835827, .170354, -.038094, 1.543313, -.411435, .082742);
+  (d3.geo.ginzburg5 = function() {
+    return projection(ginzburg5);
+  }).raw = ginzburg5;
+  var ginzburg6 = ginzburgPolyconic(5 / 6 * π, -.62636, -.0344, 0, 1.3493, -.05524, 0, .045);
+  (d3.geo.ginzburg6 = function() {
+    return projection(ginzburg6);
+  }).raw = ginzburg6;
+  function ginzburg8(λ, φ) {
+    var λ2 = λ * λ, φ2 = φ * φ;
+    return [ λ * (1 - .162388 * φ2) * (.87 - 952426e-9 * λ2 * λ2), φ * (1 + φ2 / 12) ];
+  }
+  ginzburg8.invert = function(x, y) {
+    var λ = x, φ = y, i = 50, δ;
+    do {
+      var φ2 = φ * φ;
+      φ -= δ = (φ * (1 + φ2 / 12) - y) / (1 + φ2 / 4);
+    } while (Math.abs(δ) > ε && --i > 0);
+    i = 50;
+    x /= 1 - .162388 * φ2;
+    do {
+      var λ4 = (λ4 = λ * λ) * λ4;
+      λ -= δ = (λ * (.87 - 952426e-9 * λ4) - x) / (.87 - .00476213 * λ4);
+    } while (Math.abs(δ) > ε && --i > 0);
+    return [ λ, φ ];
+  };
+  (d3.geo.ginzburg8 = function() {
+    return projection(ginzburg8);
+  }).raw = ginzburg8;
+  var ginzburg9 = ginzburgPolyconic(2.6516, -.76534, .19123, -.047094, 1.36289, -.13965, .031762);
+  (d3.geo.ginzburg9 = function() {
+    return projection(ginzburg9);
+  }).raw = ginzburg9;
   function quincuncialProjection(projectHemisphere) {
     var dx = projectHemisphere(π / 2, 0)[0] - projectHemisphere(-π / 2, 0)[0];
     function projection() {
