62aae5873883862983cfdca6c12b24b3cf9fdffb
Mike Bostock <mbostock@gmail.com>
2013-05-28 16:00:58 -0700

    Update d3.geo.projection.

--- d3.geo.projection.v0.js
+++ d3.geo.projection.v0.js
@@ -18,14 +18,35 @@
   var projection = d3.geo.projection, projectionMutator = d3.geo.projectionMutator;
   d3.geo.interrupt = function(project) {
     var lobes = [ [ [ [ -π, 0 ], [ 0, π / 2 ], [ π, 0 ] ] ], [ [ [ -π, 0 ], [ 0, -π / 2 ], [ π, 0 ] ] ] ];
-    var projection = d3.geo.projection(function(λ, φ) {
+    var bounds;
+    function forward(λ, φ) {
       var sign = φ < 0 ? -1 : +1, hemilobes = lobes[+(φ < 0)];
       for (var i = 0, n = hemilobes.length - 1; i < n && λ > hemilobes[i][2][0]; ++i) ;
       var coordinates = project(λ - hemilobes[i][1][0], φ);
       coordinates[0] += project(hemilobes[i][1][0], sign * φ > sign * hemilobes[i][0][1] ? hemilobes[i][0][1] : φ)[0];
       return coordinates;
-    });
-    var stream_ = projection.stream;
+    }
+    function reset() {
+      bounds = lobes.map(function(hemilobes) {
+        return hemilobes.map(function(lobe) {
+          var x0 = project(lobe[0][0], lobe[0][1])[0], x1 = project(lobe[2][0], lobe[2][1])[0], y0 = project(lobe[1][0], lobe[0][1])[1], y1 = project(lobe[1][0], lobe[1][1])[1], t;
+          if (y0 > y1) t = y0, y0 = y1, y1 = t;
+          return [ [ x0, y0 ], [ x1, y1 ] ];
+        });
+      });
+    }
+    if (project.invert) forward.invert = function(x, y) {
+      var hemibounds = bounds[+(y < 0)], hemilobes = lobes[+(y < 0)];
+      for (var i = 0, n = hemibounds.length; i < n; ++i) {
+        var b = hemibounds[i];
+        if (b[0][0] <= x && x < b[1][0] && b[0][1] <= y && y < b[1][1]) {
+          var coordinates = project.invert(x - project(hemilobes[i][1][0], 0)[0], y);
+          coordinates[0] += hemilobes[i][1][0];
+          return pointEqual(forward(coordinates[0], coordinates[1]), [ x, y ]) ? coordinates : null;
+        }
+      }
+    };
+    var projection = d3.geo.projection(forward), stream_ = projection.stream;
     projection.stream = function(stream) {
       var rotate = projection.rotate(), rotateStream = stream_(stream), sphereStream = (projection.rotate([ 0, 0 ]), 
       stream_(stream));
@@ -46,6 +67,7 @@
           return [ [ lobe[0][0] * π / 180, lobe[0][1] * π / 180 ], [ lobe[1][0] * π / 180, lobe[1][1] * π / 180 ], [ lobe[2][0] * π / 180, lobe[2][1] * π / 180 ] ];
         });
       });
+      reset();
       return projection;
     };
     function sphere() {
@@ -74,6 +96,9 @@
       }
       resampled.push(p1);
       return resampled;
+    }
+    function pointEqual(a, b) {
+      return Math.abs(a[0] - b[0]) < ε && Math.abs(a[1] - b[1]) < ε;
     }
     return projection;
   };
@@ -587,6 +612,44 @@
   (d3.geo.gallStereographic = function() {
     return projection(gallStereographic);
   }).raw = gallStereographic;
+  function gilbert(λ, φ) {
+    return d3.geo.orthographic.raw(λ * .5, asin(Math.tan(φ * .5)));
+  }
+  gilbert.invert = function(x, y) {
+    var coordinates = d3.geo.orthographic.raw.invert(x, y);
+    coordinates[0] *= 2;
+    coordinates[1] = 2 * Math.atan(Math.sin(coordinates[1]));
+    return coordinates;
+  };
+  function gilbertProjection() {
+    var p = projection(gilbert), e = d3.geo.equirectangular().scale(degrees).translate([ 0, 0 ]), o = d3.geo.orthographic();
+    p.stream = function(stream) {
+      stream = o.stream(stream);
+      var s = e.stream({
+        point: function(λ, φ) {
+          stream.point(λ * .5, asin(Math.tan(-φ * .5 * radians)) * degrees);
+        },
+        lineStart: function() {
+          stream.lineStart();
+        },
+        lineEnd: function() {
+          stream.lineEnd();
+        },
+        polygonStart: function() {
+          stream.polygonStart();
+        },
+        polygonEnd: function() {
+          stream.polygonEnd();
+        }
+      });
+      s.sphere = function() {
+        stream.sphere();
+      };
+      return s;
+    };
+    return d3.rebind(p, o, "rotate", "scale", "translate", "clipAngle", "precision");
+  }
+  (d3.geo.gilbert = gilbertProjection).raw = gilbert;
   function ginzburgPolyconic(a, b, c, d, e, f, g, h) {
     if (arguments.length < 8) h = 0;
     function forward(λ, φ) {
