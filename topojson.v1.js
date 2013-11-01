75ee92df85cfd32251e7afa180267ec12353254c
Mike Bostock <mbostock@gmail.com>
2013-11-01 09:40:19 -0700

    Update topojson.

--- topojson.v1.js
+++ topojson.v1.js
@@ -446,7 +446,7 @@ topojson = (function() {
   function noop() {}
 
   return {
-    version: "1.4.2",
+    version: "1.4.4",
     mesh: mesh,
     feature: featureOrCollection,
     neighbors: neighbors,
