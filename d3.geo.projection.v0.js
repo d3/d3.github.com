3d4f99e68db5a8e4cb779df319d23d196943bd8b
Jason Davies <jason@jasondavies.com>
2014-02-27 23:39:06 +0000

    Update d3.geo.projection.

--- d3.geo.projection.v0.js
+++ d3.geo.projection.v0.js
@@ -1007,7 +1007,7 @@
   function ellipticFi(φ, ψ, m) {
     var r = Math.abs(φ), i = Math.abs(ψ), sinhψ = sinh(i);
     if (r) {
-      var cscφ = 1 / Math.sin(r), cotφ2 = 1 / (Math.tan(r) * Math.tan(r)), b = -(cotφ2 + m * sinhψ * sinhψ * cscφ * cscφ - 1 + m), c = (m - 1) * cotφ2, cotλ2 = .5 * (-b + Math.sqrt(b * b - 4 * c));
+      var cscφ = 1 / Math.sin(r), cotφ2 = 1 / (Math.tan(r) * Math.tan(r)), b = -(cotφ2 + m * (sinhψ * sinhψ * cscφ * cscφ) - 1 + m), c = (m - 1) * cotφ2, cotλ2 = .5 * (-b + Math.sqrt(b * b - 4 * c));
       return [ ellipticF(Math.atan(1 / Math.sqrt(cotλ2)), m) * sgn(φ), ellipticF(Math.atan(asqrt((cotλ2 / cotφ2 - 1) / m)), 1 - m) * sgn(ψ) ];
     }
     return [ 0, ellipticF(Math.atan(sinhψ), 1 - m) * sgn(ψ) ];
@@ -1031,11 +1031,11 @@
   function guyou(λ, φ) {
     var k_ = (Math.SQRT2 - 1) / (Math.SQRT2 + 1), k = Math.sqrt(1 - k_ * k_), K = ellipticF(halfπ, k * k), f = -1;
     var ψ = Math.log(Math.tan(π / 4 + Math.abs(φ) / 2)), r = Math.exp(f * ψ) / Math.sqrt(k_), at = guyouComplexAtan(r * Math.cos(f * λ), r * Math.sin(f * λ)), t = ellipticFi(at[0], at[1], k * k);
-    return [ -t[1], sgn(φ) * (.5 * K - t[0]) ];
+    return [ -t[1], (φ >= 0 ? 1 : -1) * (.5 * K - t[0]) ];
   }
   function guyouComplexAtan(x, y) {
     var x2 = x * x, y_1 = y + 1, t = 1 - x2 - y * y;
-    return [ sgn(x) * π / 4 - .5 * Math.atan2(t, 2 * x), -.25 * Math.log(t * t + 4 * x2) + .5 * Math.log(y_1 * y_1 + x2) ];
+    return [ .5 * ((x >= 0 ? halfπ : -halfπ) - Math.atan2(t, 2 * x)), -.25 * Math.log(t * t + 4 * x2) + .5 * Math.log(y_1 * y_1 + x2) ];
   }
   function guyouComplexDivide(a, b) {
     var denominator = b[0] * b[0] + b[1] * b[1];
