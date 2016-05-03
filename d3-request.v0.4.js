fbd0a5528250f010a6dc51b6389a33d1dc8660a5
Mike Bostock <mbostock@gmail.com>
2016-05-03 11:28:25 -0700

    d3-request 0.4.6

--- d3-request.v0.4.js
+++ d3-request.v0.4.js
@@ -4,7 +4,7 @@
   (factory((global.d3_request = global.d3_request || {}),global.d3_collection,global.d3_dispatch,global.d3_dsv));
 }(this, function (exports,d3Collection,d3Dispatch,d3Dsv) { 'use strict';
 
-  var version = "0.4.5";
+  var version = "0.4.6";
 
   function request(url, callback) {
     var request,
