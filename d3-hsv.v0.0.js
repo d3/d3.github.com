6f51324485c59e648a8d823dc178669861e240b8
Mike Bostock <mbostock@gmail.com>
2016-06-08 16:16:20 -0700

    d3-hsv 0.0.1

--- d3-hsv.v0.0.js
+++ d3-hsv.v0.0.js
@@ -1,15 +1,15 @@
-// https://d3js.org/d3-hsv/ Version 0.0.0. Copyright 2016 Mike Bostock.
+// https://d3js.org/d3-hsv/ Version 0.0.1. Copyright 2016 Mike Bostock.
 (function (global, factory) {
-  typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
-  typeof define === 'function' && define.amd ? define(['exports'], factory) :
-  (factory((global.d3 = global.d3 || {})));
-}(this, function (exports) { 'use strict';
+  typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports, require('d3-color')) :
+  typeof define === 'function' && define.amd ? define(['exports', 'd3-color'], factory) :
+  (factory((global.d3 = global.d3 || {}),global.d3));
+}(this, function (exports,d3Color) { 'use strict';
 
-  function rgbConvert() {}
-  function Rgb() {}
+  var darker = 0.7;
+  var brighter = 1 / darker;
   function hsvConvert(o) {
     if (o instanceof Hsv) return new Hsv(o.h, o.s, o.v, o.opacity);
-    if (!(o instanceof Rgb)) o = rgbConvert(o);
+    if (!(o instanceof d3Color.rgb)) o = d3Color.rgb(o);
     var r = o.r / 255,
         g = o.g / 255,
         b = o.b / 255,
@@ -39,6 +39,45 @@
     this.s = +s;
     this.v = +v;
     this.opacity = +opacity;
+  }
+
+  var hsvPrototype = Hsv.prototype = hsv.prototype = Object.create(d3Color.color.prototype);
+
+  hsvPrototype.constructor = Hsv;
+
+  hsvPrototype.brighter = function(k) {
+    k = k == null ? brighter : Math.pow(brighter, k);
+    return new Hsv(this.h, this.s, this.v * k, this.opacity);
+  };
+
+  hsvPrototype.darker = function(k) {
+    k = k == null ? darker : Math.pow(darker, k);
+    return new Hsv(this.h, this.s, this.v * k, this.opacity);
+  };
+
+  hsvPrototype.rgb = function() {
+    var h = isNaN(this.h) ? 0 : this.h % 360 + (this.h < 0) * 360,
+        s = isNaN(this.h) || isNaN(this.s) ? 0 : this.s,
+        v = this.v,
+        c = v * s,
+        x = c * (1 - Math.abs((h/60)%2 - 1)),
+        m = v - c;
+    if (h < 60) return hsv2rgb(c, x, 0, m, this.opacity);
+    else if (h < 120) return hsv2rgb(x, c, 0, m, this.opacity);
+    else if (h < 180) return hsv2rgb(0, c, x, m, this.opacity);
+    else if (h < 240) return hsv2rgb(0, x, c, m, this.opacity);
+    else if (h < 300) return hsv2rgb(x, 0, c, m, this.opacity);
+    return new hsv2rgb(c, 0, x, m, this.opacity);
+  };
+
+  hsvPrototype.displayable = function() {
+    return (0 <= this.s && this.s <= 1 || isNaN(this.s))
+        && (0 <= this.v && this.v <= 1)
+        && (0 <= this.opacity && this.opacity <= 1);
+  };
+
+  function hsv2rgb(r1, g1, b1, m, a) {
+    return d3Color.rgb((r1 + m) * 255, (g1 + m) * 255, (b1 + m) * 255, a);
   }
 
   exports.hsv = hsv;
