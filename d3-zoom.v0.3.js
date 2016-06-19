677ca377d22d56cf8fe40868a156a8ac1c55acfc
Mike Bostock <mbostock@gmail.com>
2016-06-19 09:40:59 -0700

    d3-zoom 0.3.1

--- d3-zoom.v0.3.js
+++ d3-zoom.v0.3.js
@@ -1,4 +1,4 @@
-// https://d3js.org/d3-zoom/ Version 0.3.0. Copyright 2016 Mike Bostock.
+// https://d3js.org/d3-zoom/ Version 0.3.1. Copyright 2016 Mike Bostock.
 (function (global, factory) {
   typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports, require('d3-dispatch'), require('d3-drag'), require('d3-interpolate'), require('d3-selection'), require('d3-transition')) :
   typeof define === 'function' && define.amd ? define(['exports', 'd3-dispatch', 'd3-drag', 'd3-interpolate', 'd3-selection', 'd3-transition'], factory) :
