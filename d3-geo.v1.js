94f51c6ddf720ccdf28461d31d0238500906d769
Mike Bostock <mbostock@gmail.com>
2016-08-02 14:45:47 -0700

    d3-geo 1.2.2

--- d3-geo.v1.js
+++ d3-geo.v1.js
@@ -1,4 +1,4 @@
-// https://d3js.org/d3-geo/ Version 1.2.1. Copyright 2016 Mike Bostock.
+// https://d3js.org/d3-geo/ Version 1.2.2. Copyright 2016 Mike Bostock.
 (function (global, factory) {
   typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports, require('d3-array')) :
   typeof define === 'function' && define.amd ? define(['exports', 'd3-array'], factory) :
