fbdc1fe8d637284bd9045f64bc603fd7b68a0407
Mike Bostock <mbostock@gmail.com>
2016-10-24 14:44:50 -0700

    d3-fetch 0.0.2

--- d3-fetch.v0.js
+++ d3-fetch.v0.js
@@ -1,4 +1,4 @@
-// https://d3js.org/d3-fetch/ Version 0.0.1. Copyright 2016 Mike Bostock.
+// https://d3js.org/d3-fetch/ Version 0.0.2. Copyright 2016 Mike Bostock.
 (function (global, factory) {
   typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports, require('d3-dsv')) :
   typeof define === 'function' && define.amd ? define(['exports', 'd3-dsv'], factory) :
@@ -13,15 +13,25 @@ var text = function(url) {
   return fetch(url).then(responseText);
 };
 
-var json = function(url) {
-  return text(url).then(function(response) {
-    return JSON.parse(response);
-  });
-};
-
 var csv = function(url, row) {
   return text(url).then(function(response) {
     return d3Dsv.csvParse(response, row);
+  });
+};
+
+var image = function(url, anonymous) {
+  return new Promise(function(resolve, reject) {
+    var image = new Image;
+    if (anonymous) image.crossOrigin = "anonymous";
+    image.onerror = reject;
+    image.onload = function() { resolve(image); };
+    image.src = url;
+  });
+};
+
+var json = function(url) {
+  return text(url).then(function(response) {
+    return JSON.parse(response);
   });
 };
 
@@ -31,9 +41,10 @@ var tsv = function(url, row) {
   });
 };
 
+exports.csv = csv;
+exports.image = image;
 exports.json = json;
 exports.text = text;
-exports.csv = csv;
 exports.tsv = tsv;
 
 Object.defineProperty(exports, '__esModule', { value: true });
