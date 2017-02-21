af3f641159005024f9bc365845a810c1add0d82b
Mike Bostock <mbostock@gmail.com>
2017-02-20 23:01:54 -0800

    d3-selection-multi 1.0.1

--- d3-selection-multi.v1.js
+++ d3-selection-multi.v1.js
@@ -1,94 +1,94 @@
-// https://github.com/d3/d3-selection-multi Version 1.0.0. Copyright 2016 Mike Bostock.
+// https://github.com/d3/d3-selection-multi Version 1.0.1. Copyright 2017 Mike Bostock.
 (function (global, factory) {
-  typeof exports === 'object' && typeof module !== 'undefined' ? factory(require('d3-selection'), require('d3-transition')) :
-  typeof define === 'function' && define.amd ? define(['d3-selection', 'd3-transition'], factory) :
-  (factory(global.d3,global.d3));
-}(this, function (d3Selection,d3Transition) { 'use strict';
+	typeof exports === 'object' && typeof module !== 'undefined' ? factory(require('d3-selection'), require('d3-transition')) :
+	typeof define === 'function' && define.amd ? define(['d3-selection', 'd3-transition'], factory) :
+	(factory(global.d3,global.d3));
+}(this, (function (d3Selection,d3Transition) { 'use strict';
 
-  function attrsFunction(selection, map) {
-    return selection.each(function() {
-      var x = map.apply(this, arguments), s = d3Selection.select(this);
-      for (var name in x) s.attr(name, x[name]);
-    });
-  }
+function attrsFunction(selection$$1, map) {
+  return selection$$1.each(function() {
+    var x = map.apply(this, arguments), s = d3Selection.select(this);
+    for (var name in x) s.attr(name, x[name]);
+  });
+}
 
-  function attrsObject(selection, map) {
-    for (var name in map) selection.attr(name, map[name]);
-    return selection;
-  }
+function attrsObject(selection$$1, map) {
+  for (var name in map) selection$$1.attr(name, map[name]);
+  return selection$$1;
+}
 
-  function selection_attrs(map) {
-    return (typeof map === "function" ? attrsFunction : attrsObject)(this, map);
-  }
+var selection_attrs = function(map) {
+  return (typeof map === "function" ? attrsFunction : attrsObject)(this, map);
+};
 
-  function stylesFunction(selection, map, priority) {
-    return selection.each(function() {
-      var x = map.apply(this, arguments), s = d3Selection.select(this);
-      for (var name in x) s.style(name, x[name], priority);
-    });
-  }
+function stylesFunction(selection$$1, map, priority) {
+  return selection$$1.each(function() {
+    var x = map.apply(this, arguments), s = d3Selection.select(this);
+    for (var name in x) s.style(name, x[name], priority);
+  });
+}
 
-  function stylesObject(selection, map, priority) {
-    for (var name in map) selection.style(name, map[name], priority);
-    return selection;
-  }
+function stylesObject(selection$$1, map, priority) {
+  for (var name in map) selection$$1.style(name, map[name], priority);
+  return selection$$1;
+}
 
-  function selection_styles(map, priority) {
-    return (typeof map === "function" ? stylesFunction : stylesObject)(this, map, priority == null ? "" : priority);
-  }
+var selection_styles = function(map, priority) {
+  return (typeof map === "function" ? stylesFunction : stylesObject)(this, map, priority == null ? "" : priority);
+};
 
-  function propertiesFunction(selection, map) {
-    return selection.each(function() {
-      var x = map.apply(this, arguments), s = d3Selection.select(this);
-      for (var name in x) s.property(name, x[name]);
-    });
-  }
+function propertiesFunction(selection$$1, map) {
+  return selection$$1.each(function() {
+    var x = map.apply(this, arguments), s = d3Selection.select(this);
+    for (var name in x) s.property(name, x[name]);
+  });
+}
 
-  function propertiesObject(selection, map) {
-    for (var name in map) selection.property(name, map[name]);
-    return selection;
-  }
+function propertiesObject(selection$$1, map) {
+  for (var name in map) selection$$1.property(name, map[name]);
+  return selection$$1;
+}
 
-  function selection_properties(map) {
-    return (typeof map === "function" ? propertiesFunction : propertiesObject)(this, map);
-  }
+var selection_properties = function(map) {
+  return (typeof map === "function" ? propertiesFunction : propertiesObject)(this, map);
+};
 
-  function attrsFunction$1(transition, map) {
-    return transition.each(function() {
-      var x = map.apply(this, arguments), t = d3Selection.select(this).transition(transition);
-      for (var name in x) t.attr(name, x[name]);
-    });
-  }
+function attrsFunction$1(transition$$1, map) {
+  return transition$$1.each(function() {
+    var x = map.apply(this, arguments), t = d3Selection.select(this).transition(transition$$1);
+    for (var name in x) t.attr(name, x[name]);
+  });
+}
 
-  function attrsObject$1(transition, map) {
-    for (var name in map) transition.attr(name, map[name]);
-    return transition;
-  }
+function attrsObject$1(transition$$1, map) {
+  for (var name in map) transition$$1.attr(name, map[name]);
+  return transition$$1;
+}
 
-  function transition_attrs(map) {
-    return (typeof map === "function" ? attrsFunction$1 : attrsObject$1)(this, map);
-  }
+var transition_attrs = function(map) {
+  return (typeof map === "function" ? attrsFunction$1 : attrsObject$1)(this, map);
+};
 
-  function stylesFunction$1(transition, map, priority) {
-    return transition.each(function() {
-      var x = map.apply(this, arguments), t = d3Selection.select(this).transition(transition);
-      for (var name in x) t.style(name, x[name], priority);
-    });
-  }
+function stylesFunction$1(transition$$1, map, priority) {
+  return transition$$1.each(function() {
+    var x = map.apply(this, arguments), t = d3Selection.select(this).transition(transition$$1);
+    for (var name in x) t.style(name, x[name], priority);
+  });
+}
 
-  function stylesObject$1(transition, map, priority) {
-    for (var name in map) transition.style(name, map[name], priority);
-    return transition;
-  }
+function stylesObject$1(transition$$1, map, priority) {
+  for (var name in map) transition$$1.style(name, map[name], priority);
+  return transition$$1;
+}
 
-  function transition_styles(map, priority) {
-    return (typeof map === "function" ? stylesFunction$1 : stylesObject$1)(this, map, priority == null ? "" : priority);
-  }
+var transition_styles = function(map, priority) {
+  return (typeof map === "function" ? stylesFunction$1 : stylesObject$1)(this, map, priority == null ? "" : priority);
+};
 
-  d3Selection.selection.prototype.attrs = selection_attrs;
-  d3Selection.selection.prototype.styles = selection_styles;
-  d3Selection.selection.prototype.properties = selection_properties;
-  d3Transition.transition.prototype.attrs = transition_attrs;
-  d3Transition.transition.prototype.styles = transition_styles;
+d3Selection.selection.prototype.attrs = selection_attrs;
+d3Selection.selection.prototype.styles = selection_styles;
+d3Selection.selection.prototype.properties = selection_properties;
+d3Transition.transition.prototype.attrs = transition_attrs;
+d3Transition.transition.prototype.styles = transition_styles;
 
-}));
+})));
