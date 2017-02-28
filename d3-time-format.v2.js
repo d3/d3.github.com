bcad789a2189aed15f5c1df61f1754272c3d1f5f
Mike Bostock <mbostock@gmail.com>
2017-02-28 09:09:22 -0800

    d3-time-format 2.0.4

--- d3-time-format.v2.js
+++ d3-time-format.v2.js
@@ -1,8 +1,8 @@
-// https://d3js.org/d3-time-format/ Version 2.0.3. Copyright 2016 Mike Bostock.
+// https://d3js.org/d3-time-format/ Version 2.0.4. Copyright 2017 Mike Bostock.
 (function (global, factory) {
-  typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports, require('d3-time')) :
-  typeof define === 'function' && define.amd ? define(['exports', 'd3-time'], factory) :
-  (factory((global.d3 = global.d3 || {}),global.d3));
+	typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports, require('d3-time')) :
+	typeof define === 'function' && define.amd ? define(['exports', 'd3-time'], factory) :
+	(factory((global.d3 = global.d3 || {}),global.d3));
 }(this, (function (exports,d3Time) { 'use strict';
 
 function localDate(d) {
