ba60f7f80ff205df206b9fd40833c4046f31b16c
Mike Bostock <mbostock@gmail.com>
2017-03-10 09:56:41 -0800

    d3-polygon 1.0.3

--- d3-polygon.v1.js
+++ d3-polygon.v1.js
@@ -1,8 +1,8 @@
-// https://d3js.org/d3-polygon/ Version 1.0.2. Copyright 2016 Mike Bostock.
+// https://d3js.org/d3-polygon/ Version 1.0.3. Copyright 2017 Mike Bostock.
 (function (global, factory) {
-  typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
-  typeof define === 'function' && define.amd ? define(['exports'], factory) :
-  (factory((global.d3 = global.d3 || {})));
+	typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
+	typeof define === 'function' && define.amd ? define(['exports'], factory) :
+	(factory((global.d3 = global.d3 || {})));
 }(this, (function (exports) { 'use strict';
 
 var area = function(polygon) {
