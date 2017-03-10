a4a4203fb95b74439ce8fdc694df4381862aa0a3
Mike Bostock <mbostock@gmail.com>
2017-03-10 10:10:02 -0800

    d3-drag 1.0.4

--- d3-drag.v1.js
+++ d3-drag.v1.js
@@ -1,4 +1,4 @@
-// https://d3js.org/d3-drag/ Version 1.0.3. Copyright 2017 Mike Bostock.
+// https://d3js.org/d3-drag/ Version 1.0.4. Copyright 2017 Mike Bostock.
 (function (global, factory) {
 	typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports, require('d3-dispatch'), require('d3-selection')) :
 	typeof define === 'function' && define.amd ? define(['exports', 'd3-dispatch', 'd3-selection'], factory) :
