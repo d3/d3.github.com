cbe5a9932b8b6c737b863b1737eef0856047ee4e
Mike Bostock <mbostock@gmail.com>
2017-03-10 10:17:32 -0800

    d3-array 1.1.1

--- d3-array.v1.js
+++ d3-array.v1.js
@@ -1,4 +1,4 @@
-// https://d3js.org/d3-array/ Version 1.1.0. Copyright 2017 Mike Bostock.
+// https://d3js.org/d3-array/ Version 1.1.1. Copyright 2017 Mike Bostock.
 (function (global, factory) {
 	typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
 	typeof define === 'function' && define.amd ? define(['exports'], factory) :
