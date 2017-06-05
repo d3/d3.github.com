bccc9e61049340bbe32ad8a354e8e9f4a6923056
Mike Bostock <mbostock@gmail.com>
2017-06-05 08:06:33 -0700

    d3-timer 1.0.6

--- d3-timer.v1.js
+++ d3-timer.v1.js
@@ -1,4 +1,4 @@
-// https://d3js.org/d3-timer/ Version 1.0.5. Copyright 2017 Mike Bostock.
+// https://d3js.org/d3-timer/ Version 1.0.6. Copyright 2017 Mike Bostock.
 (function (global, factory) {
 	typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
 	typeof define === 'function' && define.amd ? define(['exports'], factory) :
@@ -15,7 +15,7 @@ var clockLast = 0;
 var clockNow = 0;
 var clockSkew = 0;
 var clock = typeof performance === "object" && performance.now ? performance : Date;
-var setFrame = typeof requestAnimationFrame === "function" ? requestAnimationFrame : function(f) { setTimeout(f, 17); };
+var setFrame = typeof window === "object" && window.requestAnimationFrame ? window.requestAnimationFrame.bind(window) : function(f) { setTimeout(f, 17); };
 
 function now() {
   return clockNow || (setFrame(clearNow), clockNow = clock.now() + clockSkew);
