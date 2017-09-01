2cdf4edd4cc6b000a70dfbbf147fca25acf7af66
Mike Bostock <mbostock@gmail.com>
2017-09-01 06:47:05 -0700

    d3-dsv 1.0.7

--- d3-dsv.v1.js
+++ d3-dsv.v1.js
@@ -1,8 +1,8 @@
-// https://d3js.org/d3-dsv/ Version 1.0.6. Copyright 2017 Mike Bostock.
+// https://d3js.org/d3-dsv/ Version 1.0.7. Copyright 2017 Mike Bostock.
 (function (global, factory) {
 	typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
 	typeof define === 'function' && define.amd ? define(['exports'], factory) :
-	(factory((global.d3 = {})));
+	(factory((global.d3 = global.d3 || {})));
 }(this, (function (exports) { 'use strict';
 
 var EOL = {};
