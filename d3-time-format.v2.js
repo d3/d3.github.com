b9a903b564c126bd7c7cabe6370ef4d37448fab6
Mike Bostock <mbostock@gmail.com>
2017-11-21 16:49:28 -0500

    d3-time-format 2.1.1

--- d3-time-format.v2.js
+++ d3-time-format.v2.js
@@ -1,4 +1,4 @@
-// https://d3js.org/d3-time-format/ Version 2.1.0. Copyright 2017 Mike Bostock.
+// https://d3js.org/d3-time-format/ Version 2.1.1. Copyright 2017 Mike Bostock.
 (function (global, factory) {
 	typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports, require('d3-time')) :
 	typeof define === 'function' && define.amd ? define(['exports', 'd3-time'], factory) :
