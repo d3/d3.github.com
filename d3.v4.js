25ca2ca4e2820f45419c0eb83283b414e6ed2abb
Mike Bostock <mbostock@gmail.com>
2017-12-26 13:52:23 -0800

    d3 4.12.2

--- d3.v4.js
+++ d3.v4.js
@@ -1,11 +1,11 @@
-// https://d3js.org Version 4.12.1. Copyright 2017 Mike Bostock.
+// https://d3js.org Version 4.12.2. Copyright 2017 Mike Bostock.
 (function (global, factory) {
 	typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
 	typeof define === 'function' && define.amd ? define(['exports'], factory) :
 	(factory((global.d3 = global.d3 || {})));
 }(this, (function (exports) { 'use strict';
 
-var version = "4.12.1";
+var version = "4.12.2";
 
 function ascending(a, b) {
   return a < b ? -1 : a > b ? 1 : a >= b ? 0 : NaN;
