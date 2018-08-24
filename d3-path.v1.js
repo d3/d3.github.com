b1bca2a88c745cf03010bccd321e9b3e9a945e93
Mike Bostock <mbostock@gmail.com>
2018-08-24 14:17:16 -0700

    d3-path 1.0.7

--- d3-path.v1.js
+++ d3-path.v1.js
@@ -1,4 +1,4 @@
-// https://d3js.org/d3-path/ v1.0.6 Copyright 2018 Mike Bostock
+// https://d3js.org/d3-path/ v1.0.7 Copyright 2018 Mike Bostock
 (function (global, factory) {
 typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
 typeof define === 'function' && define.amd ? define(['exports'], factory) :
