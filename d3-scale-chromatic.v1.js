88a5b77eec50726d189c68853b505b2524a14b4e
Mike Bostock <mbostock@gmail.com>
2018-08-24 14:29:05 -0700

    d3-scale-chromatic 1.3.3

--- d3-scale-chromatic.v1.js
+++ d3-scale-chromatic.v1.js
@@ -1,4 +1,4 @@
-// https://d3js.org/d3-scale-chromatic/ v1.3.2 Copyright 2018 Mike Bostock
+// https://d3js.org/d3-scale-chromatic/ v1.3.3 Copyright 2018 Mike Bostock
 (function (global, factory) {
 typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports, require('d3-interpolate'), require('d3-color')) :
 typeof define === 'function' && define.amd ? define(['exports', 'd3-interpolate', 'd3-color'], factory) :
