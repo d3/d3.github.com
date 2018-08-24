2c632364e9fb942313ee0c8d4e54c19d64962ef7
Mike Bostock <mbostock@gmail.com>
2018-08-24 11:54:26 -0700

    d3-selection 1.3.1

--- d3-selection.v1.js
+++ d3-selection.v1.js
@@ -1,8 +1,8 @@
-// https://d3js.org/d3-selection/ Version 1.3.0. Copyright 2018 Mike Bostock.
+// https://d3js.org/d3-selection/ v1.3.1 Copyright 2018 Mike Bostock
 (function (global, factory) {
-	typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
-	typeof define === 'function' && define.amd ? define(['exports'], factory) :
-	(factory((global.d3 = global.d3 || {})));
+typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
+typeof define === 'function' && define.amd ? define(['exports'], factory) :
+(factory((global.d3 = global.d3 || {})));
 }(this, (function (exports) { 'use strict';
 
 var xhtml = "http://www.w3.org/1999/xhtml";
