dda284eaf09c6ed9761f8d22f1b09ca860f469cd
Mike Bostock <mbostock@gmail.com>
2018-08-24 14:35:10 -0700

    d3-scale 2.1.2

--- d3-scale.v2.js
+++ d3-scale.v2.js
@@ -1,4 +1,4 @@
-// https://d3js.org/d3-scale/ v2.1.1 Copyright 2018 Mike Bostock
+// https://d3js.org/d3-scale/ v2.1.2 Copyright 2018 Mike Bostock
 (function (global, factory) {
 typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports, require('d3-collection'), require('d3-array'), require('d3-interpolate'), require('d3-format'), require('d3-time'), require('d3-time-format')) :
 typeof define === 'function' && define.amd ? define(['exports', 'd3-collection', 'd3-array', 'd3-interpolate', 'd3-format', 'd3-time', 'd3-time-format'], factory) :
