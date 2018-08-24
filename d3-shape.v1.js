4f401895773a234b70f057806e2cc8172332fedb
Mike Bostock <mbostock@gmail.com>
2018-08-24 14:29:49 -0700

    d3-shape 1.2.2

--- d3-shape.v1.js
+++ d3-shape.v1.js
@@ -1,4 +1,4 @@
-// https://d3js.org/d3-shape/ v1.2.1 Copyright 2018 Mike Bostock
+// https://d3js.org/d3-shape/ v1.2.2 Copyright 2018 Mike Bostock
 (function (global, factory) {
 typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports, require('d3-path')) :
 typeof define === 'function' && define.amd ? define(['exports', 'd3-path'], factory) :
