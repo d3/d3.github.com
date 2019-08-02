b55cedfea0c6e7320bf2095c48612eb2376183cd
Mike Bostock <mbostock@gmail.com>
2019-08-02 15:06:11 -0700

    d3-brush 1.1.0-rc.3

--- d3-brush.v1.js
+++ d3-brush.v1.js
@@ -1,4 +1,4 @@
-// https://d3js.org/d3-brush/ v1.1.0-rc.2 Copyright 2019 Mike Bostock
+// https://d3js.org/d3-brush/ v1.1.0-rc.3 Copyright 2019 Mike Bostock
 (function (global, factory) {
 typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports, require('d3-dispatch'), require('d3-drag'), require('d3-interpolate'), require('d3-selection'), require('d3-transition')) :
 typeof define === 'function' && define.amd ? define(['exports', 'd3-dispatch', 'd3-drag', 'd3-interpolate', 'd3-selection', 'd3-transition'], factory) :
@@ -237,7 +237,7 @@ function brush$1(dim) {
                 i = d3Interpolate.interpolate(selection0, selection1);
 
             function tween(t) {
-              state.selection = t === 1 && selection1 !== null ? null : i(t);
+              state.selection = t === 1 && selection1 === null ? null : i(t);
               redraw.call(that);
               emit.brush();
             }
