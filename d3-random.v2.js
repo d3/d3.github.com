53ed20c31d8f6169297703baf2411b781af172dc
Mike Bostock <mbostock@gmail.com>
2019-11-16 11:08:36 -0800

    d3-random 2.0.1

--- d3-random.v2.js
+++ d3-random.v2.js
@@ -1,4 +1,4 @@
-// https://d3js.org/d3-random/ v2.0.0 Copyright 2019 Mike Bostock
+// https://d3js.org/d3-random/ v2.0.1 Copyright 2019 Mike Bostock
 (function (global, factory) {
 typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
 typeof define === 'function' && define.amd ? define(['exports'], factory) :
