9255470f4d3e469fe92bc73f0b85c610487915e7
Philippe Rivière <fil@rezo.net>
2020-07-23 13:05:59 +0200

    d3-selection 1.4.2

--- d3-selection.v1.js
+++ d3-selection.v1.js
@@ -1,4 +1,4 @@
-// https://d3js.org/d3-selection/ v1.4.1 Copyright 2019 Mike Bostock
+// https://d3js.org/d3-selection/ v1.4.2 Copyright 2020 Mike Bostock
 (function (global, factory) {
 typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
 typeof define === 'function' && define.amd ? define(['exports'], factory) :
