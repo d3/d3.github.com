a04e1942a8c7f3774fb3515a398d15378f0242a4
Mike Bostock <mbostock@gmail.com>
2021-04-24 15:21:57 -0700

    d3-format 3.0.0-rc.2

--- d3-format.v3.js
+++ d3-format.v3.js
@@ -1,4 +1,4 @@
-// https://d3js.org/d3-format/ v3.0.0-rc.1 Copyright 2021 Mike Bostock
+// https://d3js.org/d3-format/ v3.0.0-rc.2 Copyright 2021 Mike Bostock
 (function (global, factory) {
 typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
 typeof define === 'function' && define.amd ? define(['exports'], factory) :
