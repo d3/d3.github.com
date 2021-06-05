092ab452acfd92f6426887ad61847e589843e99c
Mike Bostock <mbostock@gmail.com>
2021-06-05 11:23:54 -0700

    d3-path 3.0.1

--- d3-path.v3.js
+++ d3-path.v3.js
@@ -1,4 +1,4 @@
-// https://d3js.org/d3-path/ v3.0.0 Copyright 2021 Mike Bostock
+// https://d3js.org/d3-path/ v3.0.1 Copyright 2015-2021 Mike Bostock
 (function (global, factory) {
 typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
 typeof define === 'function' && define.amd ? define(['exports'], factory) :
