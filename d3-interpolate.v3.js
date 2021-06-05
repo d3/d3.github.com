9ba4c9bf1b8ff2f3baa7cba1a12f4cf2f7381010
Mike Bostock <mbostock@gmail.com>
2021-06-05 11:38:44 -0700

    d3-interpolate 3.0.1

--- d3-interpolate.v3.js
+++ d3-interpolate.v3.js
@@ -1,4 +1,4 @@
-// https://d3js.org/d3-interpolate/ v3.0.0 Copyright 2021 Mike Bostock
+// https://d3js.org/d3-interpolate/ v3.0.1 Copyright 2010-2021 Mike Bostock
 (function (global, factory) {
 typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports, require('d3-color')) :
 typeof define === 'function' && define.amd ? define(['exports', 'd3-color'], factory) :
